8aa1c014a622374695eb6831952015d7f219248c
Anezijane <ahnnieanezi@gmail.com>
2024-08-25 22:15:03 +0100

    add props

--- src/index.js
+++ src/index.js
@@ -4,13 +4,31 @@ import ReactDOM from 'react-dom/client';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import Layout from './Components/Layout';
 import ErrorPage from './pages/ErrorPage';
+import Home from './pages/Home';
+import PostDetail from './pages/PostDetail';
+import Register from './pages/Register';
+import Login from './pages/Login';
+import Authors from './pages/Authors';
+import CreatePost from './pages/CreatePost';
+import EditPost from './pages/EditPost';
+import UserProfile from './pages/UserProfile'
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Layout />,
-    errorElement: <ErrorPage />
-
+    errorElement: <ErrorPage />,
+    children: [
+      {index: true, element: <Home />},
+      {path: 'post/id', element: <PostDetail />},
+      {path: 'register', element: <Register />},
+      {path: 'login', element: <Login />},
+      {path: 'profile/:id', element: <UserProfile />},
+      {path: 'authors', element: <Authors />},
+      {path: 'create', element: <CreatePost />},
+      {path: 'posts/:id/edit', element: <EditPost />},
+        
+    ]
   }
 ])
 
