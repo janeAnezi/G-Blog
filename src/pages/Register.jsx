5ce931532d80dc0125ed958bf2ca6fcba86b83dd
Anezijane <ahnnieanezi@gmail.com>
2024-08-29 13:20:28 +0100

    add form to register component style

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Register() {
   const [userData, setuserData] = useState({
@@ -12,14 +13,25 @@ export default function Register() {
   }
 
   return (
-    <section className='register'>
-      <div className="container">
-        <h2>Sign Up</h2>
+    <section className='register-section'>
+      <div>
+      
+        <div className="container">
+          <h2>Sign Up</h2>
+        </div>
+        <form className='form register-form'>
+          <p className='form-err-msg'>This is the error message</p>
+          <input type="text" placeholder='Full Name' name='name' value={userData.name} onChange={changeInputHandler}/>
+          <input type="email" placeholder='Email' name='email' value={userData.email} onChange={changeInputHandler}/>
+          <input type="password" placeholder='Password' name='password' value={userData.password} onChange={changeInputHandler}/>
+          <input type="password" placeholder='Confirm Password' name='confirm-password' value={userData.password} onChange={changeInputHandler}/>
+
+          <button type='submit' className='btn-primary'>Register</button>
+        </form>
+
+        <small>Already have an account? <Link to={'/login'}>SignIn</Link> </small>
+        
       </div>
-      <form className='form register-form'>
-        <p className='form-err-msg'>This is the error message</p>
-        <input type="text" placeholder='Full Name' name='name' value={userData.name} onChange={changeInputHandler}/>
-      </form>
     </section>
   )
 }
