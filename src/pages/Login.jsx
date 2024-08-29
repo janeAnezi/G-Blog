b68054fb2fea1543b4e4fbef498beafd7539b29f
Anezijane <ahnnieanezi@gmail.com>
2024-08-29 14:49:44 +0100

    add content to login component

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+import img1 from '../images/earth-science.jpg'
 
 export default function Login() {
+  const [userData, setuserData] = useState({
+    email: "",
+    password: ""
+  })
+  const changeInputHandler = (e) => {
+    setuserData({...userData, [e.target.name]: e.target.value})
+  }
+
   return (
-    <div>
-      <h4>Login</h4>
-    </div>
+    <section className='register-section'>
+      <div className='form-main'>
+        <div className="container">
+          <h2>LogIn</h2>
+        </div>
+        <form className='form register-form'>
+          <p className='form-err-msg'>This is the error message</p>
+          <input type="email" placeholder='Email' name='email' value={userData.email} onChange={changeInputHandler}/>
+          <input type="password" placeholder='Password' name='password' value={userData.password} onChange={changeInputHandler}/>
+
+          <button type='submit' className='btn-primary formbtn'>Register</button>
+        </form>
+
+        <small>Don't have an account? <Link to={'/register'} className='link'>Register</Link> </small>
+        
+      </div>
+      <img src={img1} alt="" />
+    </section>
   )
 }
+
