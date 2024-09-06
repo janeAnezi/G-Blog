ee48a22faa073452b4902195e8d06ca08f2fb5fa
Anezijane <ahnnieanezi@gmail.com>
2024-09-06 13:48:16 +0100

    add error middleware and error handler module

---
+++ middleware/errorMiddleware.js
@@ -0,0 +1,21 @@
+// unsorported (404) routes
+
+const notFound = (req, res, next) => {
+    const error = new Error(`Not Found - ${req.originalUrl}`)
+    res.status(404);
+    next(error);
+}
+
+
+// middleware to handle errors
+const errorHandler = (err, req, res, next) => {
+    if(res.headerSent) {
+        return next(err);
+    }
+
+    res.status(err.code || 500).json({message: err.message || "An unknown error occured"})
+}
+
+
+
+module.exports = {notFound, errorHandler}
