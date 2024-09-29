09e1e0a65e50325e1ff7bd6941a71d499e8af1b2
Anezijane <ahnnieanezi@gmail.com>
2024-09-29 19:54:27 +0100

    add user models

--- models/userModels.js
+++ models/userModels.js
@@ -1,2 +1,9 @@
 const { schema , model} = require('mongoose')
 
+const userSchema = new schema({
+    name: {type: String, required: true},
+    email: {type: String, required: true},
+    password: {type: String, required: true},
+    avatar: {type: String},
+    posts: {type: number, default: 0},
+})
